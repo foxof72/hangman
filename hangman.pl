05c5da6f8463469f670a0d0ed83687f2ba43aa67
John Williams <johnWilliams@MacBook-Pro-4.local>
2018-02-20 17:50:14 -0500

    slightly better word getter

--- hangman.pl
+++ hangman.pl
@@ -7,20 +7,37 @@ print "Welcome to hangman!\n";
 my $dictionary = "testDictionary.txt";
 open(my $fd, "<", $dictionary)
 	or die "Can't open ", $dictionary;
-my $word = "Default Value";
-my $randomLine = int(rand(10));
-print "Random line is: ", $randomLine, "\n";
-my $counter = 0;
-while($word eq "Default Value"){
-	my $randomLine = int(rand(10));
-	while (<$fd> and ($word eq "Default Value")){
+my $word = "Default Value"; #default value is two words, impossible in a dictionary
 
+$count = `wc -l < $dictionary`;
+die "wc failed: $?" if $?;
+chomp($count);
+print $count, "\n";
+#gets a random line to retrieve the word from
+my $counter = 0;
+my $fdInit = $fd;
+#gets the word on the random line from the file
+while($word eq "Default Value"){
+	my $randomLine = int(rand($count));
+	print "Random line is: ", $randomLine, "\n";
+	while (<$fd> and ($word eq "Default Value")){
+		print "counter: ", $counter, , "\n";
 		if ($counter == $randomLine) {
 			$word = <$fd>;
 		}
 		$counter++;
 	}
-	$randomLine = int(rand(10));
 }
 chomp $word;
 print "secret word is: ", $word, "\n";
+
+#this preps the string for playing
+my @wordArray = split("", $word);
+
+print $wordArray[0], "\n";
+
+
+# play the game
+
+
+
