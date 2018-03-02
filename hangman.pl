2082eb3afff93f18d903469496b072080088e319
John Williams <johnWilliams@MacBook-Pro-4.local>
2018-03-02 11:52:23 -0500

    underscores displayed

--- hangman.pl
+++ hangman.pl
@@ -1,4 +1,5 @@
 use warnings;
+use 5.010;
 srand;
 
 print "Welcome to hangman!\n";
@@ -12,16 +13,16 @@ my $word = "Default Value"; #default value is two words, impossible in a diction
 $count = `wc -l < $dictionary`;
 die "wc failed: $?" if $?;
 chomp($count);
-print $count, "\n";
+#print $count, "\n";
 #gets a random line to retrieve the word from
 my $counter = 0;
 my $fdInit = $fd;
 #gets the word on the random line from the file
 while($word eq "Default Value"){
 	my $randomLine = int(rand($count));
-	print "Random line is: ", $randomLine, "\n";
+	#print "Random line is: ", $randomLine, "\n";
 	while (<$fd> and ($word eq "Default Value")){
-		print "counter: ", $counter, , "\n";
+		#print "counter: ", $counter, , "\n";
 		if ($counter == $randomLine) {
 			$word = <$fd>;
 		}
@@ -34,10 +35,16 @@ print "secret word is: ", $word, "\n";
 #this preps the string for playing
 my @wordArray = split("", $word);
 
-print $wordArray[0], "\n";
+#print $wordArray[0], "\n";
 
 
 # play the game
-
-
-
+my $win = 1;
+#print "length: ", $len;
+while ($win != 0){
+	for (my $var = 0; $var < scalar @wordArray; $var++) {
+		print "_ ";
+	}
+	print "\n";
+	$win = 0;
+}
