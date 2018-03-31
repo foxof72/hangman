2d1f3edbee724239de9ccff44002c605deea5b17
John Williams <johnWilliams@MacBook-Pro-4.local>
2018-03-31 12:39:08 -0400

    app keys got

--- bot/bot.pl
+++ bot/bot.pl
@@ -1,14 +1,14 @@
 use Twitter::API;
-
+require keys;
 
 #Initalizing the bot
 #THESE MUST BE FILLED IN PRIOR TO RUNTIME AND KEPT MEGA SECRET
 my $client = Twitter::API->new_with_traits(
         traits              => 'Enchilada',
-        consumer_key        => $YOUR_CONSUMER_KEY,
-        consumer_secret     => $YOUR_CONSUMER_SECRET,
-        access_token        => $YOUR_ACCESS_TOKEN,
-        access_token_secret => $YOUR_ACCESS_TOKEN_SECRET,
+        consumer_key        => $keys::consumer_key,
+        consumer_secret     => $keys::consumer_secret,
+        access_token        => $keys::access_token,
+        access_token_secret => $keys::access_token_secret,
     );
 
 my $me   = $client->verify_credentials;
